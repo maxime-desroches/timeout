8420123069a21f241383d748cd02d25a538f9145
Maxime Desroches <desroches.maxime@gmail.com>
2024-11-23 19:04:30 -0800

    better

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 
 try {
-  core.saveState("end_time", Date.now() + (core.getInput('duration') * 1000));
+  core.saveState("end_time", Date.now() + (core.getInput('timeout') * 1000));
 } catch (error) {
   core.setFailed(error.message);
 }
--- post.js
+++ post.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 
 try {
   if (core.getState("end_time") < Date.now()) {
-    core.setFailed(`THIS JOB TOOK MORE THAN ${core.getInput('duration')}s !`);
+    core.setFailed(`THIS JOB TOOK MORE THAN ${core.getInput('timeout')}s !`);
   }
 } catch (error) {
   core.setFailed(error.message);
